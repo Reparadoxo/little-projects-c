090f2568c66c623c7f1d826247e8c98c43347a12
Reparadoxo <joaofe.mokdse@gmail.com>
2025-09-08 21:48:20 -0300

    Update and rename senha.cpp to project6.cpp
    - Added notes

--- projects/project6.cpp
+++ projects/project6.cpp
@@ -8,23 +8,24 @@ int main () {
 	
 	int char1, char2, char3, char4;
 	
-	printf("Digite o primeiro caractér da senha: ");
+	printf("Digite o primeiro caractÃ©r da senha: ");
 	scanf("%d", &char1);
 
-	printf("Digite o segundo caractér da senha: ");
+	printf("Digite o segundo caractÃ©r da senha: ");
 	scanf("%d", &char2);
 	
-	printf("Digite o terceiro caractér da senha: ");
+	printf("Digite o terceiro caractÃ©r da senha: ");
 	scanf("%d", &char3);
 	
-	printf("Digite o quarto caractér da senha: ");
+	printf("Digite o quarto caractÃ©r da senha: ");
 	scanf("%d", &char4);
-	
+	//input: The algorithim ask for 4 numbers
+	//output: If the variables equals to char1= 1, char2= 2, char3= 3 and char4= 4 a message of "access allowed", if not the message "access negated" will show up
 	if (char1 == 1 and char2 == 2 and char3 == 3 and char4 == 4) {
 		printf("Acesso Permitido.");
 	} else {
 		printf("Acesso Negado.");
 	}
-	
 	return 0;
 }
+
