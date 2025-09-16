43fecee3564826bf28f18eecf91477efb01d78c9
Reparadoxo <joaofe.mokdse@gmail.com>
2025-09-16 20:05:46 -0300

    Update and rename SwitchCase.cpp to Project8.cpp
    - Added notes

--- projects/Project8.cpp
+++ projects/Project8.cpp
@@ -3,17 +3,20 @@
 
 int main() {
 	int operacao;
-	
+	//This is a simple example of the switch case condicional
 	do {
+        //The do while iteration makes a selection menu 
 		printf("### Menu ###\n");
 		printf("\n1 - Cadastrar produto\n");
 		printf("\n2 - Alterar produto\n");
 		printf("\n3 - Excluir produto\n");
 		printf("\n4 - Sair\n");
-		printf("\nEscolha a opção: ");
+		printf("\nEscolha a opÃ§Ã£o: ");
 		scanf("%d", &operacao);
 		
 		switch (operacao) {
+            // input: A number between 1 to 4
+            //output: Prints the selected number, except number 4, that exits the program
 			case 1:
 				printf("\nOpcao 1\n");
 				system("pause");
@@ -30,10 +33,12 @@ int main() {
 				printf("\nSaindo...\n");
 				system("pause");
 				break;
+            // If not recognized, the program will print an advice
 			default:
 				printf("\nOpcao Invalida\n");
 				system("pause");
-		} 
+		}
+        // Cleans the screen information 
 		system("cls");
 	}	while(operacao != 4);
 	return 0;
