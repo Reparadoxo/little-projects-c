c1a1dd62067b6543b04f1e421817f59f838194bd
Reparadoxo <joaofe.mokdse@gmail.com>
2025-11-05 11:46:46 -0300

    Update and rename projects/structC.cpp to projects/struct/structC.cpp
    - Added notes

--- projects/struct/structC.cpp
+++ projects/struct/structC.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*Here's an example of a 'Student' struct, it keeps name, age and grade*/
 struct Aluno{
 	char nome[50];
 	int idade;
@@ -7,6 +7,7 @@ struct Aluno{
 };
 
 int main() {
+	/*And here an instance of 'Aluno' called a1*/
 	struct Aluno a1;
 	printf("Digite o nome: ");
 	scanf("%s", a1.nome);
@@ -14,10 +15,11 @@ int main() {
 	scanf("%d", &a1.idade);
 	printf("Digite a nota: ");
 	scanf("%f", &a1.nota);
-	
+	/*This is for print it's data*/
 	printf("\n--- Dados do Aluno ---\n");
 	printf("Nome: %s\n", a1.nome);
 	printf("Idade: %d\n", a1.idade);
 	printf("Nota: %f\n", a1.nota);
 	return 0;
 }
+
